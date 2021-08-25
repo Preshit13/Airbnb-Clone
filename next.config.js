45607fe225f7086e65142d437b973e0d5639893a
Preshit Pimple <73753714+Preshit13@users.noreply.github.com>
2021-08-26 02:07:40 +0530

    mapbox added

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
     images:{
         domains:["links.papareact.com"],
     },
+
+    env: {
+        mapbox_key: 'pk.eyJ1IjoicHJlc2hpdDEzIiwiYSI6ImNrc3JzN3V3ODBwbTkyd25xcHpwNHJpZ3IifQ.qOrfEYPxaHX4P16X43Go1A'
+    }
 };
